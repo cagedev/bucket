adbf8716533c32d6f26701f37fdd3c7ebc0de965
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-11-28 23:55:39 +0100

    Added jsDoc

--- bucket-gui/src/components/quick-tag.js
+++ bucket-gui/src/components/quick-tag.js
@@ -1,3 +1,8 @@
+/**
+ * Generates an HTML Element and instantly populates using the options object. 
+ * @param: {string} tag     - The HTML Element to be created
+ * @param: {object} options - Object containing the properties and values to be assigned to the HTML Element
+ */
 function tag(tag, options) {
     return Object.assign(document.createElement(tag), options);
 }
