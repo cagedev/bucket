e165bbd3da1059c966f7b01cb4a8229a8528aa7d
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-11-28 23:53:54 +0100

    Removed cm5 LaTeX

--- bucket-gui/src/index.js
+++ bucket-gui/src/index.js
@@ -1,13 +1,3 @@
-// import { EditorView, basicSetup } from 'codemirror';
-// import { StreamLanguage } from '@codemirror/language';
-// import { stex } from '@codemirror/legacy-modes/mode/stex';
-
-// let myParent = document.getElementById('editor-container');
-// let editor = new EditorView({
-//     extensions: [basicSetup, StreamLanguage.define(stex)],
-//     parent: myParent,
-// })
-
 import "./headings.css"
 
 import { LatexEditor } from "./components/latex-editor";
