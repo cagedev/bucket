170fdf7e074f6b2aad6b3f22feab51464d0b0157
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-12-01 23:37:27 +0100

    Added form action changing from placeholder

--- bucket-gui/src/widgets/ajax_snippet_editor/index.js
+++ bucket-gui/src/widgets/ajax_snippet_editor/index.js
@@ -2,3 +2,13 @@ import "./container.css"
 
 import { LatexEditor } from "../../components/latex-editor";
 import { AjaxSubmit } from "../../components/ajax-submit";
+
+// TODO: This should be done in the custom form
+document.addEventListener('DOMContentLoaded', () => {
+    let action = document.getElementById('form-action-placeholder');
+    document.getElementById('snippet-editor-form').action = action.value;
+    action.addEventListener('change', () => {
+        document.getElementById('snippet-editor-form').action = action.value;
+    });
+});
+
