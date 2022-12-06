4296023c47cd0be4eeb0ec7205234a856ff35f70
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-12-06 14:07:43 +0100

    Details/summary vs. manual visibility toggle.

--- bucket-gui/src/components/snippet-editor.js
+++ bucket-gui/src/components/snippet-editor.js
@@ -18,7 +18,7 @@ const template = tag('template', {
             border: 2px solid purple;
             border-radius: 4px;
             padding: 4px;
-            display: block;
+            display: flex;
             height: 100%;
             width: 100%;
             margin-bottom: 4px;
@@ -27,13 +27,16 @@ const template = tag('template', {
 
         <div class="container">
             <form action="" id="snippet-editor-form">
-                <button id="visibility-toggle">Hide</button>
-                <input type="text" class="full-width" value="http://192.168.1.41:8888/api/snippet/2"
-                    id="form-action-placeholder" />
-                <label-selector class="full-width hideable" name="tags" value=""></label-selector>
-                <textarea class="full-width hideable" name="description"></textarea>
-                <latex-editor name="content" class="hideable" value=""></latex-editor>
-                <ajax-submit name="submit-button" class="hideable"></ajax-submit>
+                <details>
+                    <summary>snippet-title</summary>
+                    <!--//<button id="visibility-toggle">Hide</button>//-->
+                    <input type="text" class="full-width" value="http://192.168.1.41:8888/api/snippet/2"
+                        id="form-action-placeholder" />
+                    <label-selector class="full-width hideable" name="tags" value=""></label-selector>
+                    <textarea class="full-width hideable" name="description"></textarea>
+                    <latex-editor name="content" class="hideable" value=""></latex-editor>
+                    <ajax-submit name="submit-button" class="hideable"></ajax-submit>
+                </details>
             </form>
         </div>
 `});
@@ -48,36 +51,38 @@ export class SnippetEditor extends HTMLElement {
         this._formActionHolder = this.shadowRoot.getElementById('form-action-placeholder');
         this._form = this.shadowRoot.getElementById('snippet-editor-form')
         this._visibilityToggle = this.shadowRoot.getElementById('visibility-toggle')
+        this._summary = this.shadowRoot.querySelector('summary');
+
         this._componentsVisible = true;
-
-
+        this._snippetId = -1;
     }
 
     connectedCallback() {
-        // Set event listener for api target (only set after DOM loaded)
-        // BUG: If element is created after DOMContentLoaded thiis doesn't fire...
-        // Use connectedCallback
-        // document.addEventListener('DOMContentLoaded', () => {
+        // TODO: Prevent default enter handling
 
-        // Keep form action in sync with input
+        // Set initial values for form action and snippet title
         this._form.action = this._formActionHolder.value;
+        this._snippetId = this._formActionHolder.value.split('/').slice(-1)[0];
+        this._summary.innerHTML = `Snippet id=${this._snippetId}`;
+
+        // Keep form action and title in sync with input
         this._formActionHolder.addEventListener('change', () => {
             this._form.action = this._formActionHolder.value;
+            this._snippetId = this._formActionHolder.value.split('/').slice(-1)[0];
+            this._summary.innerHTML = `Snippet id=${this._snippetId}`;
         });
 
         // Toggle editor visibility
-        this._visibilityToggle.addEventListener('click', (event) => {
-            event.preventDefault();
-            this._componentsVisible = !this._componentsVisible;
-            let displayStyle = this._componentsVisible ? 'block' : 'none';
-            this.shadowRoot.querySelectorAll('.hideable').forEach((element) => {
-                element.style.display = displayStyle;
-            });
-            this._visibilityToggle.innerText = this._componentsVisible ? 'Hide' : 'Show';
-        })
-
-        // });
-
+        // The show/hide button is replaced with the details/summary-tag
+        // this._visibilityToggle.addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     this._componentsVisible = !this._componentsVisible;
+        //     let displayStyle = this._componentsVisible ? 'block' : 'none';
+        //     this.shadowRoot.querySelectorAll('.hideable').forEach((element) => {
+        //         element.style.display = displayStyle;
+        //     });
+        //     this._visibilityToggle.innerText = this._componentsVisible ? 'Hide' : 'Show';
+        // })
     }
 
 };
