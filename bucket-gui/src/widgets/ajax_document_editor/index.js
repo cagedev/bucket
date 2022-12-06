59c9346d39d1ead915e00266dce3d5ce8bf6fbfe
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-12-06 14:06:11 +0100

    Extract snippetlist from document

--- bucket-gui/src/widgets/ajax_document_editor/index.js
+++ bucket-gui/src/widgets/ajax_document_editor/index.js
@@ -3,10 +3,38 @@ import "./container.css"
 import { SnippetEditor } from "../../components/snippet-editor";
 import { tag } from "../../components/quick-tag";
 
+// TODO:
+//  - Drag-n-drop reordering
+//  - Snippet search (modal?)
+//  - Rename document
+//  - Document js component
+//  - Document db model
+
 // Set event listener for api target (only set after DOM loaded)
 document.addEventListener('DOMContentLoaded', () => {
+    // Add snippet
     document.getElementById('add-snippet').addEventListener('click', (event) => {
         event.preventDefault();
         document.getElementById('snippet-container').append(tag('snippet-editor'))
     });
+
+    // WIP: Save document
+    document.getElementById('save-document').addEventListener('click', (event) => {
+        event.preventDefault();
+
+        // WIP: Encapsulate in object 
+        // Assume default document id
+        let docId = 1;
+        // Get a list of snippet ids
+        let snippetList = [];
+        document.querySelectorAll('snippet-editor').forEach((node) => {
+            snippetList.push(node._snippetId);
+        });
+
+        // WIP: Send to endpoint
+        // DEBUG: Log to console
+        console.log(snippetList);
+    });
+
 });
+
