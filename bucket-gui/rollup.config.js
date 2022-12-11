5e4b9d8225b52e2436d5000da77d0bba3804f0d7
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-12-11 23:11:18 +0100

    Remove compilationof old widgets

--- bucket-gui/rollup.config.js
+++ bucket-gui/rollup.config.js
@@ -4,11 +4,77 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 
 export default [
+    // {
+    //     input: './src/widgets/html_latex_editor/index.js',
+
+    //     output: {
+    //         file: './build/htexedit/bundle.js',
+    //         format: 'es',
+    //     },
+
+    //     plugins: [
+    //         copy({
+    //             targets: [
+    //                 {
+    //                     src: './src/widgets/html_latex_editor/index.html',
+    //                     dest: './build/htexedit',
+    //                     transform: (contents, filename) => {
+    //                         return contents.toString().replaceAll('__TITLE__', 'Bucket LaTeX Editor')
+    //                     }
+    //                 }
+    //             ]
+    //         }),
+    //         css(),
+    //         nodeResolve(),
+    //     ],
+    // },
+    // {
+    //     input: './src/widgets/ajax_snippet_editor/index.js',
+
+    //     output: {
+    //         file: './build/atexedit/bundle.js',
+    //         format: 'es',
+    //     },
+
+    //     plugins: [
+    //         copy({
+    //             targets: [
+    //                 {
+    //                     src: './src/widgets/ajax_snippet_editor/index.html',
+    //                     dest: './build/atexedit',
+    //                 }
+    //             ]
+    //         }),
+    //         css(),
+    //         nodeResolve(),
+    //     ],
+    // },
+    // {
+    //     input: './src/widgets/ajax_document_editor/index.js',
+
+    //     output: {
+    //         file: './build/adocedit/bundle.js',
+    //         format: 'es',
+    //     },
+
+    //     plugins: [
+    //         copy({
+    //             targets: [
+    //                 {
+    //                     src: './src/widgets/ajax_document_editor/index.html',
+    //                     dest: './build/adocedit',
+    //                 }
+    //             ]
+    //         }),
+    //         css(),
+    //         nodeResolve(),
+    //     ],
+    // },
     {
-        input: './src/widgets/html_latex_editor/index.js',
+        input: './src/widgets/bucket/index.js',
 
         output: {
-            file: './build/htexedit/bundle.js',
+            file: './build/bucket/bundle.js',
             format: 'es',
         },
 
@@ -16,53 +82,8 @@ export default [
             copy({
                 targets: [
                     {
-                        src: './src/widgets/html_latex_editor/index.html',
-                        dest: './build/htexedit',
-                        transform: (contents, filename) => {
-                            return contents.toString().replaceAll('__TITLE__', 'Bucket LaTeX Editor')
-                        }
-                    }
-                ]
-            }),
-            css(),
-            nodeResolve(),
-        ],
-    },
-    {
-        input: './src/widgets/ajax_snippet_editor/index.js',
-
-        output: {
-            file: './build/atexedit/bundle.js',
-            format: 'es',
-        },
-
-        plugins: [
-            copy({
-                targets: [
-                    {
-                        src: './src/widgets/ajax_snippet_editor/index.html',
-                        dest: './build/atexedit',
-                    }
-                ]
-            }),
-            css(),
-            nodeResolve(),
-        ],
-    },
-    {
-        input: './src/widgets/ajax_document_editor/index.js',
-
-        output: {
-            file: './build/adocedit/bundle.js',
-            format: 'es',
-        },
-
-        plugins: [
-            copy({
-                targets: [
-                    {
-                        src: './src/widgets/ajax_document_editor/index.html',
-                        dest: './build/adocedit',
+                        src: './src/widgets/bucket/index.html',
+                        dest: './build/bucket',
                     }
                 ]
             }),
