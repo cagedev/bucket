35c19293f44380495c85261968c62e92e7b06db3
Mark Kooijman <kooi.barlaeus@gmail.com>
2023-01-26 13:59:49 +0100

    Rework AjaxSubmit to be a single button

--- bucket-gui/src/components/ajax-submit.js
+++ bucket-gui/src/components/ajax-submit.js
@@ -1,134 +1,185 @@
 import { tag } from "./quick-tag";
 
 
-const template = tag('template', {
+const template = tag('template', {}, {
     innerHTML: `
-    <style>
-        :host{
-            border: 2px solid purple;
-            border-radius: 4px;
-            padding: 4px;
-            display: flex;
-        }
-        button {
-            margin-right: 4px;
-        }
-    </style>
-    <button id="load-button">Load</button>
-    <button id="submit-button">Save</button>
-    <div id="status-text"></div>
-    `
-})
+    <button id="submit-button">
+      <slot></slot>
+    </button>
+    <input type="checkbox" id="auto-checkbox">`
+});
 
 /**
  * Custom HTMLElement for AJAX-submitting to an API.
  * 
- * TODO-V2: This should really be handled by a custom form.
+ * V2: This should really be handled by a custom form...?
+ * V3: The ajax-submit button should really just be the button. 
+ *     It should implement a method 'PUT', 'POST', 'GET' or 'DELETE'
  * 
  * @class
  */
 export class AjaxSubmit extends HTMLElement {
 
     static formAssociated = true;
+    static supportedMethods = ['GET', 'PUT', 'POST'];
 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this._internals = this.attachInternals();
-        // this._form = this._internals.form;
-        this._submitButton = this.shadowRoot.getElementById('submit-button');
-        this._loadButton = this.shadowRoot.getElementById('load-button');
-        this._statusText = this.shadowRoot.getElementById('status-text');
 
-        // TEMP
-        this._loadedEvent = new CustomEvent('AjaxSubmitReady', {
+        this._submitButton = this.shadowRoot.getElementById('submit-button');
+        this._autoCheckbox = this.shadowRoot.getElementById('auto-checkbox');
+
+        // Default attribute values
+        this._method = 'GET';
+        this._auto = 'off';
+
+        // Define custom events
+        // QUESTION: Define CustomEvents generally?
+        this._dataReadyEvent = new CustomEvent('DataReady', {
             bubbles: true,
             cancelable: false,
             composed: true
         });
 
+        this._statusChangedEvent = new CustomEvent('StatusChanged', {
+            bubbles: true,
+            cancelable: false,
+            composed: true
+        });
+    }
+
+    static get observedAttributes() {
+        return ['method', 'auto'];
+    }
+
+    // Set js-properties from html-attributes
+    attributeChangedCallback(name, oldValue, newValue, DEBUG = true) {
+        if (DEBUG) console.log(`<ajax-submit ${name}=${newValue}>`)
+        switch (name) {
+            case 'method':
+                if (AjaxSubmit.supportedMethods.includes(newValue)) {
+                    this._method = newValue;
+                } else {
+                    // DEBUG
+                    console.log(`Invalid attribute value of ${newValue} for ${name}, value left unchanged (${oldValue}).`)
+                }
+                break;
+
+            case 'auto':
+                if (newValue == 'on') {
+                    this._autoAvailable = true;
+                    this._auto = true;
+                    this._autoCheckbox.checked = true;
+                    this._autoCheckbox.style.visibility = 'visible';
+                } else if (newValue == 'off') {
+                    this._autoAvailable = true;
+                    this._auto = false;
+                    this._autoCheckbox.checked = false;
+                    this._autoCheckbox.style.visibility = 'visible';
+                // } else if (newValue == 'unavailable') {
+                } else {
+                    this._autoAvailable = false;
+                    this._auto = false;
+                    this._autoCheckbox.checked = false;
+                    this._autoCheckbox.style.visibility = 'hidden';
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 
     connectedCallback() {
+        // Set event listeners
         this._submitButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            let payload = Object.fromEntries(Array.from(this._internals.form.elements).map((element) => {
-                console.log(element.name, element.value)
-                return [element.name, element.value]
-            }));
-            this.sendPayload(payload);
-        });
-        this._loadButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            this.fetchData();
+            event.preventDefault(); // required?
+            this.submit();
         });
 
-        // TEMPORARY
-        this.dispatchEvent(this._loadedEvent);
+        // Fire automatically on creation if auto=true
+        if (this._auto) {
+            this.submit();
+        }
     }
 
-    // TODO: Refactor data management to parent (SnippetEditor)
-    sendPayload(payload) {
+    // Send current form data to form.action endpoint according to defined method 
+    submit() {
+        // (currently) supported methods
+        const bodyMethods = ['PUT'];
+
+        // use form target
         let target = this._internals.form.action;
-        console.log('sendPayload()->')
-        console.log(payload)
-        fetch(target, {
-            method: 'PUT',
+
+        // Define payload skeleton
+        // TODO: Include token
+        let payload = {
+            method: this._method,
             headers: {
                 "Content-type": "application/json;charset=UTF-8"
             },
-            body: JSON.stringify(payload)
-        }).then((response) => {
-            payload = response.json();
-            this.setStatus(`${response.statusText})`);
-            return payload;
-        }).then((json) => {
-            console.log(json);
-            this.setStatus(`Saved (Last modified: ${json.last_modified})`);
-        }).catch((error) => {
-            this.setStatus(error);
-        })
-    }
+        }
 
-    // TODO: Refactor data management to parent (SnippetEditor)
-    fetchData() {
-        console.log('fetchData()');
-        let target = this._internals.form.action;
-        fetch(target, {
-            method: 'GET',
-            headers: {
-                "Content-type": "application/json;charset=UTF-8"
-            },
-        }).then((response) => {
+        // Provide a body only when required
+        if (bodyMethods.includes(this._method)) {
+            payload['body'] = JSON.stringify(this.gatherPayload());
+        }
+
+        fetch(target, payload).then((response) => {
+            // Retrieve return payload
             let payload = response.json();
-            console.log(response.statusText);
-            this.setStatus(`${response.statusText})`);
+
+            // Update status
+            this.passStatus(`${response.statusText}`);
+
             return payload;
         }).then((json) => {
-            console.log(json);
-            this.setStatus(`Loaded (Last modified: ${json.last_modified})`);
-            this.loadData(json);
+            // Update status and data
+            switch (this._method) {
+                case 'GET':
+                    console.log('GET-case')
+                    this.passStatus(`Loaded (Last modified: ${json.last_modified})`);
+                    this.passData(json);
+                    break;
+
+                case 'PUT':
+                    console.log('PUT-case')
+                    this.setStatus(`Saved (Last modified: ${json.last_modified})`);
+                    break;
+
+                default:
+                    break;
+            }
         }).catch((error) => {
-            this.setStatus(error);
+            this.passStatus(error);
         })
     }
 
-    // Status should be a seperate element
-    setStatus(status) {
-        this._statusText.innerHTML = status;
+    /**
+     *  Gather form elements values into key-value object
+     */
+    gatherPayload() {
+        return Object.fromEntries(Array.from(this._internals.form.elements).map((element) => {
+            return [element.name, element.value]
+        }));
     }
 
-    loadData(payload) {
-        Array.from(this._internals.form.elements).forEach((element) => {
-            // Set data if there is a coressponding form element 
-            console.log(element.name)
-            // BUG: If field is empty this evaluates to False
-            if (payload[element.name] || payload[element.name] == '') {
-                element.value = payload[element.name];
-            }
-        });
+    /* Pass data to parent element via 
+    ? define custom event ?
+    */
+    passData(data) {
+        console.log('passData')
+        this._dataReadyEvent.payload = data;
+        this.dispatchEvent(this._dataReadyEvent);
     }
+
+    passStatus(statusText) {
+        console.log(statusText)
+    }
+
 }
 
 // Make the "ajax-submit" tag available
